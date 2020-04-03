9b2591ccfe189080701b00e2380b685af289d248
Dorothy Palesa Nantagya <61685229+Dorothie@users.noreply.github.com>
2020-04-03 12:23:14 +0300

    Add files via upload

---
+++ Assignment.js
@@ -0,0 +1,13 @@
+let grade = 80
+if (grade >= 80){
+console.log ("You got an A")
+}
+else if (grade >=65 && grade <80){
+    console.log ("You got a B")
+}
+else if (grade >=50 && grade<65){
+        console.log ("You passed")
+}
+else if (grade < 50){
+        console.log ("You failed the test")
+}
